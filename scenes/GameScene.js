4cc7cdf0dbafb1a25cd2a7605dd52df689a48c70
ｵｶｵ <aotakokeshi@gmail.com>
2026-06-21 11:48:07 +0900

    Update GameScene.js

--- scenes/GameScene.js
+++ scenes/GameScene.js
@@ -19,6 +19,27 @@ export default class GameScene extends Phaser.Scene {
             0x228B22
         );
 
+        // プレイヤー1を長方形で描画
+        // 左側に配置
+        this.add.rectangle(
+            300,    // x座標
+            560,    // y座標
+            40,     // 幅
+            80,     // 高さ
+            0x0000ff // 青色
+        );
+
+        // プレイヤー1のラベル
+        this.add.text(
+            270,
+            500,
+            "P1",
+            {
+                fontSize: "24px",
+                color: "#ffffff"
+            }
+        );
+
         // 仮の文字
         this.add.text(
             500,
